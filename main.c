dbb229733f98b7b5ee3314d041a5434101683664
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-09-26 20:28:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ char windowname[] = "r-tr-t--r0t-r-Untitled Game-r-t0r--t-rt-r";
 bool debug = true;
 int main(void)
 {
-    if (debug) { windowname += " | DEBUG MODE"; }
+    if (debug) { InitWindow(800, 450,"UG - DEBUG MODE"); } 
+    else {
     InitWindow(800, 450, windowname[0]);
+    }
 
     while (!WindowShouldClose())
     {

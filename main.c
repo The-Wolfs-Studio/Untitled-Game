8cb3f0aece9e4b3d1e5a759f40ecc121b8bbc861
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-09-26 20:14:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
-// TODO: Start code bruh
+#include <src/raylib.h>
+
+string windowname = "r-tr-t--r0t-r-Untitled Game-r-t0r--t-rt-r";
+bool debug = true;
+int main(void)
+{
+    if (debug) { windowname += " | DEBUG MODE"; }
+    InitWindow(800, 450, windowname);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Test TEXT HERE", 190, 200, 20, BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}

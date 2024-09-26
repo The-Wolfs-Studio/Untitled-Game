cd7c3fe7ceacb140e729a5900391ed157fe87879
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-09-26 20:30:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "raylib.h"
 
-char windowname[] = "r-tr-t--r0t-r-Untitled Game-r-t0r--t-rt-r";
+const char *windowname = "r-tr-t--r0t-r-Untitled Game-r-t0r--t-rt-r";
 bool debug = true;
 int main(void)
 {
     if (debug) { InitWindow(800, 450,"UG - DEBUG MODE"); } 
     else {
-    InitWindow(800, 450, windowname[0]);
+    InitWindow(800, 450, windowname);
     }
 
     while (!WindowShouldClose())

d583062b5e562516ea96dee2083921c7e72f524e
Ayman <87582743+mastergive514@users.noreply.github.com>
2024-09-30 16:22:14 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,34 @@
 
 const char *windowname = "r-tr-t--r0t-r-Untitled Game-r-t0r--t-rt-r";
 bool debug = true;
+
+bool nottrolledyet = true;
 int main(void)
 {
     if (debug) { InitWindow(800, 450,"UG - DEBUG MODE"); } 
     else {
     InitWindow(800, 450, windowname);
     }
+    
 
     while (!WindowShouldClose())
     {
+        
         BeginDrawing();
+            if (nottrolledyet) {
             ClearBackground(RAYWHITE);
+            
             DrawText("Test TEXT HERE", 190, 200, 20, BLACK);
+            sleep(2);
+            ClearBackground(BLACK);
+            nottrolledyet = false;
+
+            }
+
+            else { 
+            DrawText("Test TEXT HEREEEEEEEEEEEE", 200, 200, 20, BLACK);
+            
+            }
         EndDrawing();
     }
 
